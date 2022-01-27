f2000f96bea022f895271e76d0c87fab5eecae6f
vaishnavi patil <vp397264@gmail.com>
2022-01-27 23:20:35 +0530

    bitwise-operators

---
+++ Bitwise-Operators/src/OddEven.java
@@ -0,0 +1,9 @@
+public class OddEven {
+    public static void main(String[] args) {
+        int n = 67;
+        System.out.println(isOdd(n));
+    }
+    private static boolean isOdd(int n){
+        return(n&1)==1;
+    }
+}
