7d74bb57055beb602d312ef8ff60f7baff001e69
vaishnavi patil <vp397264@gmail.com>
2022-01-30 22:15:19 +0530

    recursion

---
+++ Recursion/src/CountZeros.java
@@ -0,0 +1,19 @@
+public class CountZeros {
+    public static void main(String[] args) {
+        System.out.println(count(3006678));
+    }
+    static int count(int n){
+        return helper(n, 0);
+    }
+    private static int helper(int n, int c){
+        if(n==0){
+
+            return c;
+        }
+        int rem = n%10;
+        if(rem==0){
+            return helper(n/10, c+1);
+        }
+        return helper(n/10 , c);
+    }
+}
