0d785fbdc6bd8d7ffdf7beb493e921c02e9d2173
vaishnavi patil <vp397264@gmail.com>
2022-02-07 18:59:00 +0530

    triangle2

---
+++ Recursion/src/Triangle2.java
@@ -0,0 +1,21 @@
+public class Triangle2 {
+
+        public static void main(String[] args) {
+            triangle2(4,0);
+        }
+        static void triangle2(int r,int c){
+            if(r==0){
+                return;
+            }
+            if(c<r){
+
+                triangle2(r,c+1);
+                System.out.print("*");
+            }else{
+
+                triangle2(r-1,0);
+                System.out.println();
+            }
+        }
+    }
+
