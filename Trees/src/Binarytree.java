74b34f445de795d8abfbc8c720f7ca998d60d4cc
vaishnavi patil <vp397264@gmail.com>
2022-04-07 22:42:03 +0530

    changes

--- Trees/src/Binarytree.java
+++ Trees/src/Binarytree.java
@@ -6,7 +6,7 @@ public class Binarytree {
 
         Node(int data){
             this.data=data;
-            this.left=null;
+            this.left= null;
             this.right=null;
         }
 
