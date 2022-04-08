c16bb2abe3b1345c1f390950c454e8de6570b6d7
vaishnavi patil <vp397264@gmail.com>
2022-04-08 15:39:02 +0530

    trees

--- Trees/src/Binarytree.java
+++ Trees/src/Binarytree.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Binarytree {
     static class Node{
         int data;
@@ -26,10 +29,69 @@ public class Binarytree {
         }
     }
 
+    public static void preorder(Node root){
+        if(root==null){
+            return;
+        }
+        System.out.println(root.data+" ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+
+    public static void inorder(Node root){
+        if(root==null){
+            return;
+        }
+        inorder(root.left);
+        System.out.println(root.data+" ");
+        inorder(root.right);
+    }
+    public static void postorder(Node root){
+        if(root==null){
+            return;
+        }
+        postorder(root.left);
+        postorder(root.right);
+        System.out.println(root.data+"");
+    }
+    public static void levelorder(Node root){
+        if(root==null){
+            return;
+        }
+        Queue<Node> q=new LinkedList<>();
+        q.add(root);
+        q.add(null);
+
+        while (!q.isEmpty()){
+            Node currNode=q.remove();
+            if(currNode==null){
+                System.out.println();
+                if(q.isEmpty()){
+                    break;
+                }else{
+                    q.add(null);
+                }
+            }else{
+                System.out.println(currNode.data + "");
+                if(currNode.left !=null){
+                    q.add(currNode.left);
+                }
+                if(currNode.right !=null){
+                    q.add(currNode.right);
+                }
+            }
+
+        }
+    }
+
     public static void main(String[] args) {
         int nodes[] ={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         BinaryTree tree=new BinaryTree();
         Node root=tree.buildTree(nodes);
-        System.out.println(root.data);
+       // System.out.println(root.data);
+        // preorder(root);
+        //inorder(root);
+       // postorder(root);
+        levelorder(root);
     }
 }
