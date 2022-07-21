b21edecc69ee3c84d762a5631149f9283b530cd5
vaishnavi patil <vp397264@gmail.com>
2022-07-21 22:00:49 +0530

    new

--- Questions/src/Array/ConcatenationOfArray.java
+++ Questions/src/Array/ConcatenationOfArray.java
@@ -1,12 +1,12 @@
-package Array;
-
-public class ConcatenationOfArray {
-    public int[] getConcatenation(int[] nums) {
-        int n = nums.length;
-        int[] ans = new int[n * 2];
-        for (int i = 0; i < n; i++) {
-            ans[i + n] = nums[i];
-            return ans;
-        }
-    }
-}
+//package Array;
+//
+//public class ConcatenationOfArray {
+//    public int[] getConcatenation(int[] nums) {
+//        int n = nums.length;
+//        int[] ans = new int[n * 2];
+//        for (int i = 0; i < n; i++) {
+//            ans[i + n] = nums[i];
+//            return ans;
+//        }
+//    }
+//}
