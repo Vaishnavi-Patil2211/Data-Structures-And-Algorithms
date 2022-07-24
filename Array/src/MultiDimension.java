119687125f9a27f5f13993a5b8a36f3896ede2ff
vaishnavi patil <vp397264@gmail.com>
2022-07-24 22:31:15 +0530

    new

--- Array/src/MultiDimension.java
+++ Array/src/MultiDimension.java
@@ -12,14 +12,14 @@ public class MultiDimension {
                 arr[row][col]=in.nextInt();
             }
         }
-//
-//        for(int row=0;row<arr.length;row++){
-////            for(int col=0;col<arr[row].length;col++){
-////                System.out.print(arr[row][col]+" ");
-////            }
-////            System.out.println();
-//            System.out.println(Arrays.toString(arr[row]));
-//        }
+
+        for(int row=0;row<arr.length;row++){
+            for(int col=0;col<arr[row].length;col++){
+                System.out.print(arr[row][col]+" ");
+            }
+            System.out.println();
+            System.out.println(Arrays.toString(arr[row]));
+        }
         for(int[] a:arr){
             System.out.println(Arrays.toString(a));
         }
