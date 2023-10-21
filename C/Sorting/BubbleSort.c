36629f740bd298c030e4f6cab2774e6bca3c4db1
Atharva <atharvapatil7120@gmail.com>
2023-10-21 23:50:09 +0530

    cpp code update

--- C/Sorting/BubbleSort.c
+++ C/Sorting/BubbleSort.c
@@ -1,23 +1,40 @@
-#include<stdio.h>
-void main(){
-    int n,num[100],i,j,temp;
-    printf("How Many Number You Want to Insert: ");
-    scanf("%d",&n);
-    for(i=0;i<n;i++){
-    	printf("Enter a Number: ");
-    	scanf("%d",&num[i]);
-	}
-	for(i=0;i<n;i++){
-		for(j=0;j<n;j++){
-			if(num[j]>num[j+1]){
-				temp=num[j];
-				num[j]=num[j+1];
-				num[j+1]=temp;
-			}
-		}		
-	}
-	printf("Sorted Data: \n");
-	for(i=0;i<n;i++){
-		printf("%d\t",num[i]);
-	}
+#include <stdio.h>
+
+void bubbleSort(int num[], int n) {
+    if (n == 1) {
+        return;
+    }
+
+    // Perform a single pass of Bubble Sort
+    for (int i = 0; i < n - 1; i++) {
+        if (num[i] > num[i + 1]) {
+            // Swap num[i] and num[i + 1]
+            int temp = num[i];
+            num[i] = num[i + 1];
+            num[i + 1] = temp;
+        }
+    }
+
+    // Recursive call for the remaining elements
+    bubbleSort(num, n - 1);
+}
+
+int main() {
+    int n, num[100];
+    printf("How Many Numbers You Want to Insert: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        printf("Enter a Number: ");
+        scanf("%d", &num[i]);
+    }
+
+    bubbleSort(num, n);
+
+    printf("Sorted Data:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d\t", num[i]);
+    }
+
+    return 0;
 }
