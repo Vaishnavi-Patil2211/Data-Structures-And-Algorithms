ea7da8d8022cdf64658d764adeb1fd5cbe6b6917
Atharva <atharvapatil7120@gmail.com>
2023-10-21 23:40:15 +0530

    Java code update

--- Java/linear search/src/FindNumber.java
+++ Java/linear search/src/FindNumber.java
@@ -1,26 +1,20 @@
 public class FindNumber {
     public static void main(String[] args) {
-        int[] nums={12,23,34,45,56,67,78,89};
-        int target=45;
-        boolean ans=linearsearch(nums,target);
+        int[] nums = {12, 23, 34, 45, 56, 67, 78, 89};
+        int target = 45;
+        boolean ans = linearSearch(nums, target);
         System.out.println(ans);
     }
-    //search in the array:return the index if items found
-    //otherwise the item not found return -1
-    static boolean linearsearch(int[] arr,int target){
-        if (arr.length==0){
-            return false;
-        }
-        //run a for loop
-        for(int i =0; i<arr.length;i++){
-        //check for element at every index if it is = target
 
-        if(arr[i] == target){
-            return true;
+    // Search in the array: return true if the item is found, otherwise return false
+    static boolean linearSearch(int[] arr, int target) {
+        for (int num : arr) {
+            if (num == target) {
+                return true;
+            }
         }
-        }
-        //this line will exicute if none of the return statements above have executed
-        //hence the target not found
+        // If the loop completes without finding the target, return false
         return false;
     }
 }
+
